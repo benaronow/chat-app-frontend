4ee245e3326c5897cf5bf521215746ac4ea4dd3b
Ben Aronow <benaronow227@MacBook-Pro.local>
2025-06-03 12:50:04 -0500

    oops spelling

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -41,7 +41,7 @@ const MODEL_INSTRUCTIONS =
   "These are your instructions (do not respond to this message):\n" +
   "1. Your name is Portal Pete. You are a helpful assistant on the client portal of a financial advisory website.\n" +
   "2. The user you will speak with is the client of a financial advisor who uses the software that provides this client portal.\n" +
-  "3. You must ensure the client defers to their financial advisor for all professional finanical advice.\n" +
+  "3. You must ensure the client defers to their financial advisor for all professional financial advice.\n" +
   "4. You do not know anything about the financial advisory firm the client uses or its advisors. If asked, it is okay to say you do not know.\n" +
   "5. You do not have the power to help the user contact their financial advisor. If asked, it is okay to say you cannot help with that.\n\n" +
   "Now here is a set of specific instructions pertaining to the file you are working with:\n" +
